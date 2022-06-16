43d5967c27a45276c2318811bff0253bcea9cb01
cristhian ochoa <cristhho@gmail.com>
2022-06-15 23:54:03 -0500

    add navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 
 import { Layout } from '../components/layout';
+import { Navbar } from '../components/ui';
 
 const HomePage: NextPage = () => {
   return (
     <Layout title='Listado de pokemones'>
+      <Navbar />
       <h1>Pokedex</h1>
     </Layout>
   )
