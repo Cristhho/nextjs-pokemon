422f79fd2e832e7cb930deb308e1db781689895d
cristhian ochoa <cristhho@gmail.com>
2022-06-18 17:45:48 -0500

    meta tags

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,6 +7,8 @@ type LayoutProps = {
   title?: string
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: FC<PropsWithChildren<LayoutProps>> = ({ children, title }) => {
   return (
     <>
@@ -15,6 +17,10 @@ export const Layout: FC<PropsWithChildren<LayoutProps>> = ({ children, title }) 
         <meta name='author' content='Christian Ochoa' />
         <meta name='description' content='Información sobre el pokemon ____' />
         <meta name='keywords' content='____,pokemon,pokedex' />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta property="og:description" content={`Esta es la página sobre ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar />
